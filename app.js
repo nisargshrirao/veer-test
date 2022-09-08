507b9b59c593ff040376a536d39235aac78b6b75
nisarg <nisargshrirao2014@gmail.com>
2022-09-08 12:07:01 +0530

    set port no

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", router);
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("listeniing at port:3000")
 }) 
+
